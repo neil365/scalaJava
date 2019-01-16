0b4d37a586c80fbce197964b82016501e3d46311
liz <33024793@qq.com>
2019-01-16 16:00:01 +0800

    update

--- src/main/java/cn/neil/scalaJava/App.java
+++ src/main/java/cn/neil/scalaJava/App.java
@@ -1,5 +1,7 @@
 package cn.neil.scalaJava;
 
+import cn.neil.scala.TestScala;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
+    	TestScala ts = new TestScala(0);
+    	ts.clamathid();
+    	
         System.out.println( "Hello World!" );
     }
 }
